b77fdcf71dd91516531c53b24a440ded0952b364
ayd3n <ayd3n1229@gmail.com>
2022-05-25 22:30:58 -0400

    just learning the ropes.
    added basic item and block through
    registries

--- src/main/java/com/froggydev/divinity/regestries/DivinityBlocks.java
+++ src/main/java/com/froggydev/divinity/regestries/DivinityBlocks.java
@@ -1,4 +1,39 @@
 package com.froggydev.divinity.regestries;
 
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+import static com.froggydev.divinity.DivinityMod.MODID;
+
 public class DivinityBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+
+    public static final RegistryObject<Block> BONFIRE = registerBlock("bonfire",
+            () -> new Block(BlockBehaviour.Properties.of(Material.FIRE)
+                    .strength(9f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    public static <T extends Block>RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab){
+        return DivinityItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)));
+    }
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
 }
--- src/main/java/com/froggydev/divinity/regestries/DivinityItems.java
+++ src/main/java/com/froggydev/divinity/regestries/DivinityItems.java
@@ -1,4 +1,18 @@
 package com.froggydev.divinity.regestries;
 
+
+import com.froggydev.divinity.DivinityMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class DivinityItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, DivinityMod.MODID);
+    public static final RegistryObject<Item> HOLY_TORCH = ITEMS.register("holy_torch", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
