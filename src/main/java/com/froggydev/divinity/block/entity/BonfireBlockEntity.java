492bb0d938f7ac1d1937ffcf0fc4f9c22591ce4b
Her02N0one <ayd3n1229@gmail.com>
2022-06-03 10:55:52 -0400

    still trying to wrap my head around block entities

--- src/main/java/com/froggydev/divinity/block/entity/BonfireBlockEntity.java
+++ src/main/java/com/froggydev/divinity/block/entity/BonfireBlockEntity.java
@@ -1,14 +1,28 @@
 package com.froggydev.divinity.block.entity;
 
+import com.froggydev.divinity.registry.ModBlockEntityTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class BonfireBlockEntity extends SyncedBlockEntity {
     private static final int NUM_SLOTS = 1;
+    private final ItemStackHandler inventory;
 
     public BonfireBlockEntity(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState) {
-        super(blockEntityType, blockPos, blockState);
+        super(ModBlockEntityTypes.BONFIRE.get(), blockPos, blockState);
+        inventory = createHandler();
+    }
+
+    private ItemStackHandler createHandler() {
+        return new ItemStackHandler(NUM_SLOTS)
+        {
+            @Override
+            public int getSlotLimit(int slot) {
+                return 1;
+            }
+        };
     }
 }
--- src/main/java/com/froggydev/divinity/registry/ModBlockEntityTypes.java
+++ src/main/java/com/froggydev/divinity/registry/ModBlockEntityTypes.java
@@ -1,13 +1,13 @@
 package com.froggydev.divinity.registry;
 
 import com.froggydev.divinity.DivinityMod;
+import com.froggydev.divinity.block.entity.BonfireBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockEntityTypes {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPES =
             DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, DivinityMod.MODID);
-
-
 }
